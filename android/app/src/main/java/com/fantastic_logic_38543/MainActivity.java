6a39915bf11bca83556300acb986fcd7811f92b0
Crowdbotics <team@crowdbotics.com>
2023-01-28 08:41:27 +0000

    Initial commit.

---
+++ android/app/src/main/java/com/fantastic_logic_38543/MainActivity.java
@@ -0,0 +1,15 @@
+package com.fantastic_logic_38543;
+
+import com.facebook.react.ReactActivity;
+
+public class MainActivity extends ReactActivity {
+
+  /**
+   * Returns the name of the main component registered from JavaScript. This is used to schedule
+   * rendering of the component.
+   */
+  @Override
+  protected String getMainComponentName() {
+    return "fantastic_logic_38543";
+  }
+}
---
+++ options/options.js
@@ -0,0 +1,12 @@
+import { StyleSheet } from "react-native"
+
+export const globalOptions = {
+  name: "fantastic-logic-38543",
+  url: "https://fantastic-logic-38543.botics.co",
+  api: "https://fantastic-logic-38543.botics.co/api/v1"
+}
+export const modulesOptions = {
+  "@modules/app-menu": {
+    copy: "Routes available!"
+  }
+}
